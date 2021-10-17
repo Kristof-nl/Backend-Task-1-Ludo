4ddc8f25ae24e0a52f4607944cb6ed3e7ac82836
K. Hermaszewski <hermaszewski.krzysztof@gmail.com>
2021-10-17 22:00:56 +0200

    Start Fibonacci

--- Backend Task 1 Ludo/Program.cs
+++ Backend Task 1 Ludo/Program.cs
@@ -6,7 +6,56 @@ namespace Backend_Task_1_Ludo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool closeFlag = false;
+
+            while (!closeFlag)
+            {
+                Console.WriteLine("Kies een van de onderstande opties. Type een getal om een keus te makken.");
+                Console.WriteLine("1 - Circle oppervlakte");
+                Console.WriteLine("2 - Fibonacci rij");
+                Console.WriteLine("3 - Uitgang");
+
+                string choose = Console.ReadLine();
+                if (choose == "1")
+                {
+                    circleRadius();
+                }
+                else if (choose == "2")
+                {
+                    fibonacci();
+                }
+                else if (choose == "3")
+                {
+                    closeFlag = true;
+                }
+                else
+                {
+                    Console.WriteLine("Ongeldige invoer. Type een getal tussen 1 en 3.");
+                }
+
+            }
+
+            static void circleRadius()
+            {
+                int radius;
+                const double PI = 3.1415926535897931;
+
+                Console.Write("Type een circle radius om de oppervlakte te berekenen: ");
+                try
+                {
+                    radius = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("De oppervlakte is " + (radius * PI));
+                }
+                catch
+                {
+                    Console.WriteLine("Ongeldige invoer. De circle radius moet een getal zijn.");
+                }
+            }
+
+            static void fibonacci()
+            {
+
+            }
         }
     }
 }
