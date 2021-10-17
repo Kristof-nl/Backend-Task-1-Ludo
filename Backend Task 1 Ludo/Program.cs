d785cf197e777d74b88467da913ac7aecc7c8e1e
K. Hermaszewski <hermaszewski.krzysztof@gmail.com>
2021-10-17 21:37:02 +0200

    Add project files.

---
+++ Backend Task 1 Ludo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Backend_Task_1_Ludo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
